19a3119afb21c6580105ba640bda79f97ebd42f8
John Colosi <jjcolosi@gmail.com>
2017-05-16 18:48:29 -0400

    Knots working but unsubmitted

--- src/jjc/codechef/knots/Main2.java
+++ src/jjc/codechef/knots/Main2.java
@@ -4,15 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
-import jjc.codechef.knots.Turn.Kind;
+import jjc.codechef.knots.Cell.Dir;
 
 /**
  * https://www.codechef.com/problems/ACMKANPA
  */
-public class Main {
+public class Main2 {
 
 	static public void main(String[] args) throws IOException {
 		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
@@ -28,13 +27,6 @@ public class Main {
 			for (int thisRow = 0; thisRow < rows; thisRow++) {
 				matrix.applyRow(thisRow, r.readLine());
 			}
-			matrix.findEnds();
-			// System.out.format("Case %d: %s%n", caseNum,
-			// matrix.getMatrixString());
-			matrix.buildCrux();
-			// System.out.format("Case %d: %s%n", caseNum,
-			// matrix.getCruxString());
-
 			String result = matrix.isKnot() ? "knotted" : "straightened";
 			System.out.format("Case %d: %s%n", caseNum, result);
 		}
@@ -43,10 +35,6 @@ public class Main {
 }
 
 class Matrix {
-	public enum Dir {
-		UP, LEFT, DOWN, RIGHT
-	}
-
 	Cell cells[][];
 	int rows;
 	int cols;
@@ -80,132 +68,52 @@ class Matrix {
 		for (int thisRow = 0; thisRow < rows; thisRow++) {
 			if (cells[thisRow][0].c == '*') {
 				ends.add(cells[thisRow][0]);
-				// cells[thisRow][0].last = Dir.LEFT;
+				cells[thisRow][0].last = Dir.LEFT;
 			}
 			if (cells[thisRow][cols - 1].c == '*') {
 				ends.add(cells[thisRow][cols - 1]);
-				// cells[thisRow][cols - 1].last = Dir.RIGHT;
+				cells[thisRow][cols - 1].last = Dir.RIGHT;
 			}
 		}
 		for (int thisCol = 1; thisCol < cols - 1; thisCol++) {
 			if (cells[0][thisCol].c == '*') {
 				ends.add(cells[0][thisCol]);
-				// cells[0][thisCol].last = Dir.UP;
+				cells[0][thisCol].last = Dir.UP;
 			}
 			if (cells[rows - 1][thisCol].c == '*') {
 				ends.add(cells[rows - 1][thisCol]);
-				// cells[rows - 1][thisCol].last = Dir.DOWN;
+				cells[rows - 1][thisCol].last = Dir.DOWN;
 			}
 		}
 		endA = ends.get(0);
 		endB = ends.get(1);
 	}
 
-	static ArrayList<Turn> turns = null;
-
-	public String getCruxString() {
+	public String getCrux() {
 		StringBuilder out = new StringBuilder();
-		for (Turn turn : turns) {
-			out.append(turn.kind == Kind.OVER ? 'O' : 'U');
-			out.append("[" + turn.nexus + "] ");
-		}
-		return out.toString();
-	}
-
-	public String getMatrixString() {
-		StringBuilder out = new StringBuilder();
-		for (int thisRow = 0; thisRow < rows; thisRow++) {
-			for (int thisCol = 0; thisCol < cols; thisCol++) {
-				out.append(cells[thisRow][thisCol].c);
-			}
-			out.append("\n");
-		}
-		return out.toString();
-	}
-
-	public void buildCrux() {
-		turns = new ArrayList<Turn>();
 		Cell pointer = endA;
 
-		Dir last = null;
-		if (pointer.l == null) last = Dir.LEFT;
-		else if (pointer.r == null) last = Dir.RIGHT;
-		else if (pointer.u == null) last = Dir.UP;
-		else if (pointer.d == null) last = Dir.DOWN;
-
 		while (pointer != endB) {
-			// System.out.println(">>> " + pointer.id);
-
-			if (pointer.hasRight() && last != Dir.RIGHT) {
-				last = Dir.LEFT;
-				pointer = pointer.r;
-				if (pointer.c == '-') {
-					turns.add(new Turn(Kind.OVER, pointer.id));
-					pointer = pointer.r;
-				}
-				if (pointer.c == '|') {
-					turns.add(new Turn(Kind.UNDER, pointer.id));
-					pointer = pointer.r;
-				}
-			} else if (pointer.hasDown() && last != Dir.DOWN) {
-				last = Dir.UP;
-				pointer = pointer.d;
-				if (pointer.c == '-') {
-					turns.add(new Turn(Kind.UNDER, pointer.id));
-					pointer = pointer.d;
-				}
-				if (pointer.c == '|') {
-					turns.add(new Turn(Kind.OVER, pointer.id));
-					pointer = pointer.d;
-				}
-			} else if (pointer.hasLeft() && last != Dir.LEFT) {
-				last = Dir.RIGHT;
-				pointer = pointer.l;
-				if (pointer.c == '-') {
-					turns.add(new Turn(Kind.OVER, pointer.id));
-					pointer = pointer.l;
-				}
-				if (pointer.c == '|') {
-					turns.add(new Turn(Kind.UNDER, pointer.id));
-					pointer = pointer.l;
-				}
-			} else {
-				last = Dir.DOWN;
-				pointer = pointer.u;
-				if (pointer.c == '-') {
-					turns.add(new Turn(Kind.UNDER, pointer.id));
-					pointer = pointer.u;
-				}
-				if (pointer.c == '|') {
-					turns.add(new Turn(Kind.OVER, pointer.id));
-					pointer = pointer.u;
-				}
-			}
-			// System.out.println(getCruxString());
+			Cell next = null;
+			if (pointer.hasRight() && pointer.last != Dir.RIGHT) {
+				next = pointer.r;
+				if (next.c == '-') out.append('o');
+			} else if (pointer.hasDown() && pointer.last != Dir.DOWN) next = pointer.d;
+			else if (pointer.hasLeft() && pointer.last != Dir.LEFT) next = pointer.l;
+			else next = pointer.u;
 		}
-	}
 
-	public boolean isKnot() {
-		return isKnot(Kind.OVER, Kind.OVER) && isKnot(Kind.OVER, Kind.UNDER) && isKnot(Kind.UNDER, Kind.OVER)
-				&& isKnot(Kind.UNDER, Kind.UNDER);
-	}
-
-	public boolean isKnot(Kind aKind, Kind bKind) {
-		int size = turns.size();
-		int a = 0;
-		int b = size - 1;
-		HashSet<Integer> cleared = new HashSet<>();
-
-		while (a < b) {
-			if (turns.get(a).kind == aKind || cleared.contains(turns.get(a).nexus)) {
-				cleared.add(turns.get(a).nexus);
-				a++;
-			} else if (turns.get(b).kind == bKind || cleared.contains(turns.get(b).nexus)) {
-				cleared.add(turns.get(b).nexus);
-				b--;
-			} else return true;
+		List<Cell> ends = new ArrayList<Cell>();
+		for (int thisRow = 0; thisRow < rows; thisRow++) {
+			if (cells[thisRow][0].c == '*') ends.add(cells[thisRow][0]);
+			if (cells[thisRow][cols - 1].c == '*') ends.add(cells[thisRow][cols - 1]);
 		}
-		return false;
+		for (int thisCol = 1; thisCol < cols - 1; thisCol++) {
+			if (cells[0][thisCol].c == '*') ends.add(cells[0][thisCol]);
+			if (cells[rows - 1][thisCol].c == '*') ends.add(cells[rows - 1][thisCol]);
+		}
+		endA = ends.get(0);
+		endB = ends.get(1);
 	}
 
 	// public void applyRowSimple(int row, String data) {
@@ -215,8 +123,6 @@ class Matrix {
 	// }
 	// }
 
-	static private int cellId = 0;
-
 	public void applyRow(int row, String data) {
 		char[] array = data.toCharArray();
 		for (int thisCol = 0; thisCol < array.length; thisCol++) {
@@ -224,7 +130,6 @@ class Matrix {
 			if (x == '|' || x == '-' || x == '+') cells[row][thisCol].c = '*';
 			else if (x == 'I') cells[row][thisCol].c = '|';
 			else if (x == 'H') cells[row][thisCol].c = '-';
-			cells[row][thisCol].id = cellId++;
 		}
 	}
 
@@ -253,31 +158,16 @@ class Matrix {
 	// }
 }
 
-class Turn {
-	public enum Kind {
-		OVER, UNDER
-	}
-
-	public Kind kind;
-	public int nexus;
-
-	public Turn(Kind kind, int nexus) {
-		this.kind = kind;
-		this.nexus = nexus;
-	}
-
-}
-
 class Cell {
+	public enum Dir {
+		UP, LEFT, DOWN, RIGHT
+	}
 
 	public char c; // " " * | -
 	public Cell u, d, l, r;
 	public boolean cleared = false;
+	public Dir last = null;
 	public int id;
-
-	public boolean equals(Object o) {
-		return (o != null && o instanceof Cell && ((Cell) o).id == id);
-	}
 
 	public Cell() {
 		c = ' ';
